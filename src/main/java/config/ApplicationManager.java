0b70ad7fefd12050264544701f5bb202a067093f
AndrSmirnov <asmirnoff.1974@gmail.com>
2024-05-06 09:50:43 +0200

    Pom and Html

--- src/main/java/config/ApplicationManager.java
+++ src/main/java/config/ApplicationManager.java
@@ -1,13 +1,16 @@
 package config;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import pages.AlertsPage;
+import pages.HomePage;
+import pages.LeftSideMenu;
 
 import java.time.Duration;
 
@@ -16,12 +19,11 @@ public class ApplicationManager {
     static WebDriver driver;
     String browser;
 
-public  ApplicationManager (String browser){
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
-         this.browser = browser;
-}
-
-    public void init() {
+    public WebDriver init() {
         if(browser.equals("chrome")) {
             // with tools:
             //driver = new ChromeDriver();
@@ -50,7 +52,7 @@ public  ApplicationManager (String browser){
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.navigate().to("https://demoqa.com/");
-
+        return driver;
     }
 
     public void quit() {
---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,92 @@
+package pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BasePage {
+
+    WebDriver driver;
+    JavascriptExecutor js;
+    static Logger logger = LoggerFactory.getLogger(BasePage.class);
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        // with chain and @FindBy
+        PageFactory.initElements(driver, this);
+        js = (JavascriptExecutor) driver;
+    }
+
+    public void clickBase(WebElement element) {
+        element.click();
+    }
+
+    public void clickWithJSScroll(WebElement element, int x, int y) {
+        jsScroll(x, y);
+        clickBase(element);
+    }
+
+    protected void jsScroll(int x, int y) {
+        js.executeScript("window.scrollBy(" + x + "," + y + ")");
+    }
+
+    public void typeText(WebElement element, String text) {
+        element.click();
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public String getTextBase(WebElement element) {
+        return element.getText().trim();
+    }
+
+    public void pause(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected boolean isStringsEqual(String actualRes, String expectedRes) {
+        if(actualRes.equals(expectedRes)) {
+            return true;
+        } else {
+            System.out.println("actual res: " + actualRes +
+                    " expected res " + expectedRes);
+            return false;
+        }
+    }
+
+    protected Keys getKeyForOSCommandOrControl() {
+        String os = System.getProperty("os.name");
+        System.out.println("my os: " + os);
+        if(os.startsWith("Mac")) {
+            return Keys.COMMAND;
+        } else if(os.startsWith("Win")) {
+            return Keys.CONTROL;
+        }
+        return Keys.CONTROL;
+    }
+
+    protected String getAttributeBase(WebElement el, String attributeName) {
+        return el.getAttribute(attributeName);
+    }
+
+    public void hideIFrames() {
+        hideFooter();
+        hideBanner();
+    }
+
+    private void hideBanner() {
+        js.executeScript("document.getElementById('fixedban').style.display='none';");
+    }
+
+    private void hideFooter() {
+        js.executeScript("document.querySelector('footer').style.display='none';");
+    }
+}
